a35903bba3936d942ddf9578cb7ed566aadf1f5b
aleaker <alejoakerfeld@gmail.com>
2020-09-28 14:58:33 -0300

    Updates

---
+++ precache-manifest.9bef1403955cba6ba343c3c1a0becaa5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb3de04de2f1fc40175a74ffacf3b0e",
+    "url": "/lucianoBonoWeb/index.html"
+  },
+  {
+    "revision": "f89a3822ce3341d7d681",
+    "url": "/lucianoBonoWeb/static/js/2.f7a2de57.chunk.js"
+  },
+  {
+    "revision": "b9941b26ce7f87a25d1a3bc0d5ae97bc",
+    "url": "/lucianoBonoWeb/static/js/2.f7a2de57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce56cab49045cb50bcc6",
+    "url": "/lucianoBonoWeb/static/js/main.8296bff2.chunk.js"
+  },
+  {
+    "revision": "96758f75009e76643e24",
+    "url": "/lucianoBonoWeb/static/js/runtime-main.a4abedf8.js"
+  },
+  {
+    "revision": "c3d45c3568ac00a818926b400e24003f",
+    "url": "/lucianoBonoWeb/static/media/avionero.c3d45c35.jpg"
+  },
+  {
+    "revision": "6bb0a0c07d1d84c9683eba1d836f4b28",
+    "url": "/lucianoBonoWeb/static/media/laCaja1.6bb0a0c0.jpg"
+  },
+  {
+    "revision": "571639e07cc3778f7cb6c593dfd88b28",
+    "url": "/lucianoBonoWeb/static/media/laCaja2.571639e0.jpg"
+  },
+  {
+    "revision": "099b076042085f483c491ada68712be1",
+    "url": "/lucianoBonoWeb/static/media/lego1.099b0760.jpg"
+  },
+  {
+    "revision": "29e33e03b93c88507c88ee6cb6eb3802",
+    "url": "/lucianoBonoWeb/static/media/lego2.29e33e03.jpg"
+  },
+  {
+    "revision": "2e0485197eaa208aa8c0fdfc2f290c83",
+    "url": "/lucianoBonoWeb/static/media/luciano.2e048519.png"
+  },
+  {
+    "revision": "94a43aa8040dfb9d5a7fa1e6ac4b753f",
+    "url": "/lucianoBonoWeb/static/media/spotify1.94a43aa8.jpg"
+  },
+  {
+    "revision": "74d0b10d72a362d074ed599df892838b",
+    "url": "/lucianoBonoWeb/static/media/spotify2.74d0b10d.jpg"
+  }
+]);
